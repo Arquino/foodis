44e8498dc66634932b27a546fdde406cb5179ecc
Arctroid1 <arctroidrobotmink@gmail.com>
2020-05-22 20:55:48 +0200

    modification des routes

--- routes/resto.js
+++ routes/resto.js
@@ -3,9 +3,9 @@ const  router = express.Router();
 const restoCtrl = require('../controllers/resto')
 
 
+// Les routes Des restauts
  
 router.get('/', restoCtrl.getResto);  // Récupération de tous les restaurants
-
 router.get('/:id', restoCtrl.getOneResto); // Récuperation d'un restaurant
 // router.put('/:id', restoCtl.modifyResto)  modification d'un resto
 router.post('/', restoCtrl.createResto) //  Post pour creer un restaurant
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 
+
+// Les route user
 router.get('/', userCtrl.getUser);
 router.post('/signup', userCtrl.signupUser);
 router.post('/login', userCtrl.loginUser)
